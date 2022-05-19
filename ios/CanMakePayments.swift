4e36d8df4c7444681b45050e8fc7947e0c5fc07d
toda <ytoda@d-forest-corp.com>
2022-05-19 09:53:02 +0900

    set requiresMainQueueSetup false

--- ios/CanMakePayments.swift
+++ ios/CanMakePayments.swift
@@ -2,6 +2,9 @@ import StoreKit
 
 @objc(CanMakePayments)
 class CanMakePayments: NSObject {
+    @objc static func requiresMainQueueSetup() -> Bool {
+        return false
+    }
 
     @objc public func canMakePayments(_ resolve:RCTPromiseResolveBlock, withRejecter reject:RCTPromiseRejectBlock) -> Void {
       resolve(SKPaymentQueue.canMakePayments())
